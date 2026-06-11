85e7045d697cd9f1ac6085dac77307ac7b173e80
royyannaja6-tech <royyannaja6@gmail.com>
2026-06-12 01:16:02 +0700

    Prepare for Vercel

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.set('view engine','ejs');
-
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000,()=>{
-    console.log('Server berjalan di http://localhost:3000');
-});
+// Export app untuk Vercel
+module.exports = app;
+
+// Jalankan lokal
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log('Server berjalan di http://localhost:3000');
+    });
+}
